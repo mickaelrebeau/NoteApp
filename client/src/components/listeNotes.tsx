58d24b5c8d03106b949e2c2a4cff88214e4cc7fb
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-11-20 21:55:16 +0100

    💄 style: update style

--- client/src/components/listeNotes.tsx
+++ client/src/components/listeNotes.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
 } from "@/components/ui/card"
-import { deleteNote } from "@/utils/fetch";
 import { Link } from "react-router-dom";
+import { ConfirmModalNote } from "./modals/confirmModalNote";
 
 interface File {
 	id: string;
@@ -21,8 +20,8 @@ export function ListeNotes({ label, datas }: { label: { value: string, label: st
 			<h2 className="text-xl font-semibold">Notes - {label.label}</h2>
 			<div className="w-[95%] p-4 grid grid-flow-col gap-4 overflow-x-auto">
 				{datas.map((data) => (
-					<Card key={data.id}>
-						<CardContent className="p-4 flex justify-between gap-4">
+					<Card key={data.id} className="w-80">
+						<CardContent className="p-4 flex flex-col justify-between gap-4">
 							<div className="flex flex-col justify-center gap-4">
 								<h3 className="text-lg font-semibold">{data.title}</h3>
 								<p className="max-h-[150px] text-slate-500 overflow-auto">
@@ -41,19 +40,11 @@ export function ListeNotes({ label, datas }: { label: { value: string, label: st
 									))}
 								</div>
 							</div>
-							<div className="flex flex-col justify-center gap-2">
-								<Button
-									className="mt-8 w-full py-2 rounded-md text-md font-semibold text-red-500 cursor-pointer hover:text-white bg-transparent hover:bg-gradient-to-r from-amber-600 to-yellow-400 hover:shadow-2xl hover:shadow-amber-600/50"
-									onClick={() => {
-										deleteNote(data.id);
-										window.location.reload();
-									}}
-								>
-									Supprimer
-								</Button>
+							<div className="flex justify-center gap-2">
+								<ConfirmModalNote id={data.id} />
 								<Link
 									to={`/edit/${data.id}`}
-									className="block mt-8 w-full py-2 rounded-md text-md text-center font-semibold text-black dark:text-white cursor-pointer hover:text-white dark:hover:text-white bg-transparent hover:bg-gradient-to-r from-amber-600 to-yellow-400 hover:shadow-2xl hover:shadow-amber-600/50"
+									className="block mt-8 w-full py-2 rounded-md text-md text-center font-semibold text-black dark:text-white cursor-pointer hover:text-white dark:hover:text-white bg-transparent hover:bg-gradient-to-r from-amber-600 to-yellow-400"
 								>
 									Editer
 								</Link>
