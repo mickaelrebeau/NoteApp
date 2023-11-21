4ebf85844a307b64b5af91915fb189b25b38c177
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-11-21 23:11:20 +0100

    🛠️ build: prettier

--- api/src/upload/upload.controller.ts
+++ api/src/upload/upload.controller.ts
@@ -3,10 +3,10 @@ import { UploadService } from './upload.service';
 
 @Controller('files')
 export class UploadController {
-    constructor(private fileService: UploadService) { }
-    
-    @Delete(':idFile')
-    destroyFile(@Param('idFile') id: string) {
-        return this.fileService.delete(id);
-    }
+  constructor(private fileService: UploadService) {}
+
+  @Delete(':idFile')
+  destroyFile(@Param('idFile') id: string) {
+    return this.fileService.delete(id);
+  }
 }
